cf605f6781f362b6c7f5782a0a2a2e554f6067a5
PHIKOPP <philipp.kopph@gmail.com>
2025-08-22 18:24:47 +0200

    Imported springboot project

---
+++ src/main/java/pkopp/digital_assistant_service/DigitalAssistantServiceApplication.java
@@ -0,0 +1,13 @@
+package pkopp.digital_assistant_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalAssistantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalAssistantServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pkopp/digital_assistant_service/DigitalAssistantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pkopp.digital_assistant_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalAssistantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
