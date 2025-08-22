9d2a0a6380efe23abd7da04736ef46b0be97b208
PHIKOPP <philipp.kopph@gmail.com>
2025-08-22 18:44:50 +0200

    Added first simple test

--- src/test/java/pkopp/digital_assistant_service/DigitalAssistantServiceApplicationTests.java
+++ src/test/java/pkopp/digital_assistant_service/DigitalAssistantServiceApplicationTests.java
@@ -1,13 +1,35 @@
 package pkopp.digital_assistant_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DigitalAssistantServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void helloEndpoint_returns_200() throws Exception {
+		mockMvc.perform(get("/api/hello"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	void helloEndpoint_returns_response() throws Exception {
+		mockMvc.perform(get("/api/hello"))
+				.andExpect(content().string("Hello, World!"));
 	}
 
 }
